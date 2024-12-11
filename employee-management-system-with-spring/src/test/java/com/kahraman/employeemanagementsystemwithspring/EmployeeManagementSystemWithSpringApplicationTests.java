5c602ef69aa8ec623ce289c6d109b6bfdf84464c
hasan.kahraman <kahramanhasanb@gmail.com>
2024-12-11 15:09:15 +0100

    new spring project added

---
+++ employee-management-system-with-spring/src/test/java/com/kahraman/employeemanagementsystemwithspring/EmployeeManagementSystemWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kahraman.employeemanagementsystemwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
